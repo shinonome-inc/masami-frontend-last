9c6ae83bf08168d3ce5f4f78f3eb1fa259015d01
kawaguchimasami <81722546+kawaguchimasami@users.noreply.github.com>
2021-05-06 00:12:49 +0900

    Update atom-button.stories.js

--- src/stories/atom-button.stories.js
+++ src/stories/atom-button.stories.js
@@ -1,1 +1,6 @@
+import Item_Example from "../components/atoms/button.html";
+import "../components/atoms/button.scss";
 
+export default { title: "Atoms/button" }
+
+export const button= () => Item_button;
