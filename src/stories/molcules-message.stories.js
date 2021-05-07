eaa2178d08f0e9ac54a1c6a85e7ce419e06aacf3
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-05-08 01:23:43 +0900

    first commit

---
+++ src/stories/molcules-message.stories.js
@@ -0,0 +1,6 @@
+import Item_Example from "../components/molcules/message.html";
+import "../components/molecules/message.scss";
+
+export default { title: "molcules/message" }
+
+export const message = () => Item_message;
