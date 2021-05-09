2995e3b0729f3fdc1c0690c67701a1d06f9a45d0
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-05-10 02:41:59 +0900

    インデントをなおしました

---
+++ src/stories/molecules-news.stories.js
@@ -0,0 +1,6 @@
+import Item_news from "../components/molecules/news/news.html";
+import "../components/molecules/news/news.scss";
+
+export default { title: "molecules/news" }
+
+export const news = () => Item_news;
