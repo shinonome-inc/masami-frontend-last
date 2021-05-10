899f8f040f61c5e0bb92e7b82ee24faf0450bfdf
kawaguchimasami <81722546+kawaguchimasami@users.noreply.github.com>
2021-05-11 00:56:52 +0900

    Revert "first commit"

--- src/stories/molecules-news.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_news from "../components/molecules/news/news.html";
-import "../components/molecules/news/news.scss";
-
-export default { title: "molecules/news" }
-
-export const news = () => Item_news;
