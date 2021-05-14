b8d9ab4dd26f4a7ca89b6489e86801c6c9ed3970
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-05-15 04:33:57 +0900

    新しくブランチを作成しました

---
+++ src/stories/molecules-card2.stories.js
@@ -0,0 +1,6 @@
+import Item_card2 from "../components/molecules/card2/card2.html";
+import "../components/molecules/card2/card2.scss";
+
+export default { title: "molecules/card2" }
+
+export const card2 = () => Item_card2;
