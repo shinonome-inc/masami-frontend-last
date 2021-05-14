0974a7757c685043d0296aa2baf377b2e92e37ce
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-05-15 04:23:00 +0900

    ブランチを新しく切りました

---
+++ src/stories/molesules-newsmore2.stories.js
@@ -0,0 +1,6 @@
+import Item_newsmore2 from "../components/molecules/newsmore2/newsmore2.html";
+import "../components/molecules/newsmore2/newsmore2.scss";
+
+export default { title: "Molecules1/newsmore2" }
+
+export const newsmore2 = () => Item_newsmore2;
