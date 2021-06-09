7a7a2862ba062dc76049ca8d607f98f9f5c7a4f0
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-06-09 20:58:28 +0900

    新しく作成しています

--- src/stories/atom-button.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_button from "../components/atoms/button/button.html";
-import "../components/atoms/button/button.scss";
-
-export default { title: "Atoms/button" }
-
-export const button= () => Item_button;
