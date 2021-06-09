c671a20ffca671fa1a01b1d4299a0ace09dc72bd
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-06-09 20:29:13 +0900

    新しく作成しました

--- src/stories/molecules-message2.stories.js
+++
@@ -1,6 +0,0 @@
-import Item_message2 from "../components/molecules/message2/message2.html";
-import "../components/molecules/message2/message2.scss";
-
-export default { title: "molecules/message2" }
-
-export const message2 = () => Item_message2;
