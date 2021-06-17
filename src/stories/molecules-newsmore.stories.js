57416639641579b26d417273de6ef273a69df099
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-06-18 02:51:22 +0900

    訂正しました

--- src/stories/molecules-newsmore.stories.js
+++ src/stories/molecules-newsmore.stories.js
@@ -1,6 +1,6 @@
 import Item_newsmore from "../components/molecules/newsmore/newsmore.html";
 import "../components/molecules/newsmore/newsmore.scss";
 
-export default { title: "Molecules1/newsmore" }
+export default { title: "Molecules/newsmore" }
 
 export const newsmore = () => Item_newsmore;
