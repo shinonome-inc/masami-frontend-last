8984b7b93375fbd61ee30578ef411289c291b4c2
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-08-25 03:54:22 +0900

    first commit

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import Item_organismsnews from "../components/organisms/news/news.html";
+import "../components/organisms/news/news.scss";
+
+export default { title: "organisms/news" }
+
+export const organismsnews = () => Item_organismsnews;
