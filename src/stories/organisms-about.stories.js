c045e7cd3a43eac5e8568a451043a72520b233df
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-08-29 15:07:37 +0900

    first

---
+++ src/stories/organisms-about.stories.js
@@ -0,0 +1,6 @@
+import Item_organismsabout from "../components/organisms/about/about.html";
+import "../components/organisms/about/about.scss";
+
+export default { title: "organisms/about" }
+
+export const organismsabout = () => Item_organismsabout;
