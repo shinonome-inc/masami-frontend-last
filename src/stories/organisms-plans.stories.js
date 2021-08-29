268dee8d7a674a10148cef310c77955509b0fe02
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-08-29 16:02:25 +0900

    blanch 作製

---
+++ src/stories/organisms-plans.stories.js
@@ -0,0 +1,6 @@
+import Item_organismsplans from "../components/organisms/plans/plans.html";
+import "../components/organisms/plans/plans.scss";
+
+export default { title: "organisms/plans" }
+
+export const organismsplans = () => Item_organismsplans;
