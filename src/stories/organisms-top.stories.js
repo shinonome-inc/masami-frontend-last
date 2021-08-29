7fab7a927c4f12d271f7946b0cf77cf2cd89939d
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-08-29 12:18:39 +0900

    first

---
+++ src/stories/organisms-top.stories.js
@@ -0,0 +1,6 @@
+import Item_organismstop from "../components/organisms/top/top.html";
+import "../components/organisms/top/top.scss";
+
+export default { title: "organisms/top" }
+
+export const organismstop = () => Item_organismstop;
