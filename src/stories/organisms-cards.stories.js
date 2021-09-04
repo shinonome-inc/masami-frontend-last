cdcdd960aaf24c564eca86ae6babbcd2730b13cf
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-09-05 06:45:16 +0900

    新しくブランチを切りました

---
+++ src/stories/organisms-cards.stories.js
@@ -0,0 +1,6 @@
+import Item_organismscards from "../components/organisms/cards/cards.html";
+
+
+export default { title: "organisms/cards" }
+
+export const organismscards = () => Item_organismscards;
