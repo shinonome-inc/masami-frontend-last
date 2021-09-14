ff7a94d669a402922f2472d6d332a1639ab11e23
masami kawaguchi <yaoshengmei0112@gmail.com>
2021-09-14 22:06:34 +0900

    news

---
+++ src/stories/organisms-news.stories.js
@@ -0,0 +1,6 @@
+import Item_news from "../components/organisms/news/news.html";
+import "../components/organisms/news/news.scss";
+
+export default { title: "organisms/news" }
+
+export const news = () => Item_news;
